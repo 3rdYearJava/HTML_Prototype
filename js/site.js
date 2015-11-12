250099bbe125f2771f6ffdf1861d5d8cd4b0c68b
BenCookie95 <benkcooke@gmail.com>
2015-11-12 17:39:34 +0000

    Created admin panel, big changes to javascript form validation and finished request gig page

--- js/site.js
+++ js/site.js
@@ -5,133 +5,178 @@ function validateComment(){
 	warning.style.display = "block";
 	userpost.focus();
 	return false;
-}else{
-	return true;
-}
+	}else{
+		return true;
+	}
 
 };
+
 var hide = function(){
-      mistake.style.display = "none";
+      mistake.style.color = document.getElementById('mistake').innerHTML = "";
     }
+var letterNumber = /^[0-9a-zA-Z]+$/;
+var numberCheck = /^[0-9.]+$/;
+var mistakeText;
+var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
+
+
+
+function showError(name){
+	name.style.borderColor = "red";
+	name.style.borderWidth = "medium";
+	name.focus();
+	name.value = "";
+}
+
 function validateRegistration(){
-	var letterNumber = /^[0-9a-zA-Z]+$/;
-	var mistakeText;
-if (fname.value.length < 3) {
+	
+if (fname.value.length < 2) {
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left your first name blank";
 	setTimeout(hide, 5000);
-	fname.style.borderColor = "red";
-	fname.style.borderWidth = "medium";
-	fname.focus();
+	showError(fname);
+	setTimeout(function() { colourChange(fname); }, 5000);
 	return false;
-}else if(lname.value.length < 3){
+}else if(lname.value.length < 2){
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left your last name blank";
 	setTimeout(hide, 5000);
-	lname.style.borderColor = "red";
-	lname.style.borderWidth = "medium";
-	lname.focus();
-
+	showError(lname);
+	setTimeout(function() { colourChange(lname); }, 5000);
 	return false;
-}else if(email.value.length < 3){
+}else if(email.value.length < 2){
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left your email blank";
 	setTimeout(hide, 5000);
-	email.style.borderColor = "red";
-	email.style.borderWidth = "medium";
-	email.focus();
-
+	showError(email);
+	setTimeout(function() { colourChange(email); }, 5000);
 	return false;
-}else if(username.value.length < 3){
+}else if(username.value.length < 2){
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left your username blank";
 	setTimeout(hide, 5000);
-	username.style.borderColor = "red";
-	username.style.borderWidth = "medium";
-	username.focus();
-
+	showError(username);
+	setTimeout(function() { colourChange(username); }, 5000);
 	return false;
 }
 else if( !username.value.match(letterNumber)){
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your username is invalid, your username can only contain alphanumeric characters";
 	setTimeout(hide, 10000);
-	username.style.borderColor = "red";
-	username.style.borderWidth = "medium";
-	username.value = "";
-	username.focus();
+	showError(username);
+	setTimeout(function() { colourChange(username); }, 10000);
 	return false;
 }
-else if(password.value.length < 3){
+else if(password.value.length < 2){
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left your password blank";
 	setTimeout(hide, 5000);
-	password.style.borderColor = "red";
-	password.style.borderWidth = "medium";
-	password.focus();
-
+	showError(password);
+	setTimeout(function() { colourChange(password); }, 5000);
 	return false;
 }
-else if(password1.value.length < 3){
+else if(password1.value.length < 2){
 	var mistakeText = document.getElementById('mistake').innerHTML = "You left the second password field blank";
 	setTimeout(hide, 5000);
-	password1.style.borderColor = "red";
-	password1.style.borderWidth = "medium";
-	password1.focus();
+	showError(password1);
+	setTimeout(function() { colourChange(password1); }, 5000);
 	return false;
 }else if(fname.value.length > 25){
-	fname.style.borderColor = "red";
-	fname.style.borderWidth = "medium";
-	fname.focus();
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your first name is greater than 25 characters";
 	setTimeout(hide, 10000);
-	fname.value = "";
+	showError(fname);
+	setTimeout(function() { colourChange(fname); }, 10000);
 	return false;
 }else if(lname.value.length > 25){
-	lname.style.borderColor = "red";
-	lname.style.borderWidth = "medium";
-	lname.focus();
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your last name is greater than 25 characters";
 	setTimeout(hide, 10000);
-	lname.value = "";
+	showError(lname);
+	setTimeout(function() { colourChange(lname); }, 10000);
 	return false;
 }else if(email.value.length > 60){
-	email.style.borderColor = "red";
-	email.style.borderWidth = "medium";
-	email.focus();
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your email is too long";
 	setTimeout(hide, 10000);
-	email.value = "";
+	showError(email);
+	setTimeout(function() { colourChange(email); }, 10000);
 	return false;
 }else if(username.value.length > 25){
-	username.style.borderColor = "red";
-	username.style.borderWidth = "medium";
-	username.focus();
+
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your username is greater than 25 characters";
 	setTimeout(hide, 10000);
-	username.value = "";
+	showError(username);
+	setTimeout(function() { colourChange(username); }, 10000);
 	return false;
 }
 else if(password.value.length > 25){
-	password.style.borderColor = "red";
-	password.style.borderWidth = "medium";
-	password.focus();
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your password is greater than 25 characters";
 	setTimeout(hide, 10000);
-	password.value = "";
+	showError(password);
+	password1.value = ""
+	setTimeout(function() { colourChange(password); }, 10000);
 	return false;
 }
 
 else if(password.value !== password1.value){
-	password.style.borderColor = "red";
-	password.style.borderWidth = "medium";
-	password1.style.borderColor = "red";
-	password1.style.borderWidth = "medium";
-	password.value=  "";
-	password1.value = "";
+	
 	var mistakeText = document.getElementById('mistake').innerHTML = "Your passwords don't match";
 	setTimeout(hide, 10000);
-	password.focus();
+	showError(password1);
+	showError(password);
+	setTimeout(function() { colourChange(password); },10000);
+	setTimeout(function() { colourChange(password1); }, 10000);
 	return false;
 }
 else{
 	return true;
 }
 };
+
 function colourChange(this1){
 	this1.style.borderColor = "white";
+};
+
+function validateSubmission(){
+var selectedText = document.getElementById('gigDate').value;
+var selectedDate = new Date(selectedText);
+var now = new Date();
+if (band.value.length < 1) {
+	var mistakeText = document.getElementById('mistake').innerHTML = "Please choose a band";
+	setTimeout(hide, 5000);
+	showError(band);
+	setTimeout(function() { colourChange(band); }, 5000);
+	return false;
+}
+else if(description.value.length < 10){
+	var mistakeText = document.getElementById('mistake').innerHTML = "Please give the gig a description.";
+	setTimeout(hide, 5000);
+	showError(description);
+	setTimeout(function() { colourChange(description); }, 5000);
+	return false;
+}
+else if (venue.value.length < 1) {
+	var mistakeText = document.getElementById('mistake').innerHTML = "Please choose a venue";
+	setTimeout(hide, 5000);
+	showError(venue);
+	setTimeout(function() { colourChange(venue); }, 5000);
+	return false;
+}
+else if( !price.value.match(numberCheck)){
+	var mistakeText = document.getElementById('mistake').innerHTML = "You can only use numbers when suggesting a price";
+	setTimeout(hide, 10000);
+	showError(price);
+	setTimeout(function() { colourChange(price); }, 10000);
+	return false;
+}
+else if (gigDate.value.length < 1) {
+	var mistakeText = document.getElementById('mistake').innerHTML = "Please choose a date";
+	setTimeout(hide, 5000);
+	showError(gigDate);
+	setTimeout(function() { colourChange(gigDate); }, 5000);
+	return false;
+}
+else if (selectedDate < now) {
+	var mistakeText = document.getElementById('mistake').innerHTML = "Your date is in the past";
+	setTimeout(hide, 5000);
+	showError(gigDate);
+	setTimeout(function() { colourChange(gigDate); }, 5000);
+	return false;
+}
+else{
+	return true;
+}
+
 };
